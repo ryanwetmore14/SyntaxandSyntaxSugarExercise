72293dff6210b1e7855e0bde8b81500578e475e1
ryan wetmore <ryanwetmore1014@gmil.com>
2024-05-01 15:25:10 -0400

    completed syntax and sugar exercise

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace SyntaxAndSyntaxSugarExercise
+﻿using System.Runtime.CompilerServices;
+
+namespace SyntaxAndSyntaxSugarExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than 9" : $"{answer} is greater than 9";
+            Console.WriteLine(response);
+
+            var answer2 = 20;
+            var response2 = (answer2 > 9) ? $"{answer2} is greater than 9" : $"{answer2} is less than 9";
+            Console.WriteLine(response2); 
+
         }
     }
 }
+
